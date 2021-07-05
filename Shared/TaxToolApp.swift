4eb40d78992f646836e8d7c46ae04f60d323df21
thoamsy <c6swift@gmail.com>
2021-07-05 22:47:29 +0800

    Initial Commit

---
+++ Shared/TaxToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxToolApp.swift
+//  Shared
+//
+//  Created by yk on 2021/7/5.
+//
+
+import SwiftUI
+
+@main
+struct TaxToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
